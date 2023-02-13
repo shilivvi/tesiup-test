21b2ecf697b888e500beb6a5811ba98c362bb850
shilivvi <vanya.ivanov161@gmail.com>
2023-02-13 19:06:09 +0300

    Init wp theme

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * tesiup-test functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package tesiup-test
+ */
+
+if ( ! defined( '_S_VERSION' ) ) {
+	define( '_S_VERSION', '1.0.0' );
+}
