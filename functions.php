469346a988059e07e7d48a46e36600a8522ee823
shilivvi <vanya.ivanov161@gmail.com>
2023-02-17 01:47:24 +0300

    Added Tesiup_Walker_Nav_Menu class

--- functions.php
+++ functions.php
@@ -1,12 +1,10 @@
 <?php
-/**
- * tesiup-test functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package tesiup-test
- */
 
-if ( ! defined( '_S_VERSION' ) ) {
-	define( '_S_VERSION', '1.0.0' );
+if (!defined('_S_VERSION')) {
+    define('_S_VERSION', '1.0.0');
+}
+
+// Load classes
+foreach (glob(dirname(__FILE__) . '/inc/classes/*.php') as $class) {
+    require_once $class;
 }
---
+++ inc/classes/Tesiup_Walker_Nav_Menu.php
@@ -0,0 +1,99 @@
+<?php
+
+class Tesiup_Walker_Nav_Menu
+{
+
+    public function start_lvl(&$output, $depth = 0, $args = null)
+    {
+        if (isset($args->item_spacing) && 'discard' === $args->item_spacing) {
+            $t = '';
+            $n = '';
+        } else {
+            $t = "\t";
+            $n = "\n";
+        }
+        $indent = str_repeat($t, $depth);
+
+        // Default class.
+        $classes = array('sub-menu');
+
+        if (!empty($args->sub_menu_class)) {
+            foreach (explode(' ', $args->sub_menu_class) as $item) {
+                $classes[] = $item;
+            }
+        }
+
+        $class_names = implode(' ', apply_filters('nav_menu_submenu_css_class', $classes, $args, $depth));
+        $class_names = $class_names ? ' class="' . esc_attr($class_names) . '"' : '';
+
+        $output .= "{$n}{$indent}<ul$class_names>{$n}";
+    }
+
+    public function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0)
+    {
+
+        if (isset($args->item_spacing) && 'discard' === $args->item_spacing) {
+            $t = '';
+            $n = '';
+        } else {
+            $t = "\t";
+            $n = "\n";
+        }
+        $indent = ($depth) ? str_repeat($t, $depth) : '';
+
+        $classes = empty($item->classes) ? array() : (array)$item->classes;
+        $classes[] = 'menu-item-' . $item->ID;
+
+        $args = apply_filters('nav_menu_item_args', $args, $item, $depth);
+
+        $class_names = join(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args, $depth));
+
+        if (!empty($args->item_class)) {
+            $class_names = $class_names ? ' class="' . esc_attr($class_names) . ' ' . esc_attr($args->item_class) . '"' : '';
+        } else {
+            $class_names = $class_names ? ' class="' . esc_attr($class_names) . '"' : '';
+        }
+
+        $id = apply_filters('nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args, $depth);
+        $id = $id ? ' id="' . esc_attr($id) . '"' : '';
+
+        $output .= $indent . '<li' . $id . $class_names . '>';
+
+        $atts = array();
+        $atts['title'] = !empty($item->attr_title) ? $item->attr_title : '';
+        $atts['target'] = !empty($item->target) ? $item->target : '';
+        $atts['rel'] = !empty($item->xfn) ? $item->xfn : '';
+        $atts['href'] = !empty($item->url) ? $item->url : '';
+
+        $atts = apply_filters('nav_menu_link_attributes', $atts, $item, $args, $depth);
+
+        $attributes = '';
+        foreach ($atts as $attr => $value) {
+            if (!empty($value)) {
+                $value = ('href' === $attr) ? esc_url($value) : esc_attr($value);
+                $attributes .= ' ' . $attr . '="' . $value . '"';
+            }
+        }
+
+        if (!empty($args->link_class)) {
+            $attributes = ' class="' . esc_attr($args->link_class) . '"' . $attributes;
+        }
+
+        $title = apply_filters('the_title', $item->title, $item->ID);
+        $title = apply_filters('nav_menu_item_title', $title, $item, $args, $depth);
+
+        $item_output = $args->before;
+        $item_output .= '<a' . $attributes . '>';
+        $item_output .= $args->link_before . $title . $args->link_after;
+
+        if (!empty($args->drop_down_icon) && $args->walker->has_children) {
+            $item_output .= $args->drop_down_icon;
+        }
+
+        $item_output .= '</a>';
+        $item_output .= $args->after;
+
+        $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
+    }
+
+}
