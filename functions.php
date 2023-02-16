05486306380e6e545085aea34986af14b6d8ffd7
shilivvi <vanya.ivanov161@gmail.com>
2023-02-17 01:48:33 +0300

    Added registration nav menus and styles

--- functions.php
+++ functions.php
@@ -8,3 +8,23 @@ if (!defined('_S_VERSION')) {
 foreach (glob(dirname(__FILE__) . '/inc/classes/*.php') as $class) {
     require_once $class;
 }
+
+// Register nav menus
+function tesiup_register_nav_menus()
+{
+    register_nav_menus([
+        'top_header_menu' => 'Top header menu',
+        'header_menu' => 'Header menu',
+        'mobile_header_menu' => 'Mobile header menu',
+    ]);
+}
+
+add_action('after_setup_theme', 'tesiup_register_nav_menus');
+
+// Load styles
+function tesiup_enqueue_style()
+{
+    wp_enqueue_style('tesiup', get_stylesheet_uri());
+}
+
+add_action('wp_enqueue_scripts', 'tesiup_enqueue_style');
