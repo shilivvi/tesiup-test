3a4c27ef9c7950cae662c712bfd578006874b0bb
shilivvi <vanya.ivanov161@gmail.com>
2023-02-17 16:45:11 +0300

    Added warning section

---
+++ blocks/warning-section/warning-section.php
@@ -0,0 +1,12 @@
+<section class="warning">
+    <div class="warning__container container">
+        <div class="warning__wrap">
+            <i class="fas fa-exclamation warning__icon" aria-label="hidden"></i>
+            <div class="warning__content">
+                <p class="warning__text">
+                    <?php the_field('text'); ?>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
