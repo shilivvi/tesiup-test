13ddb2f4ec3b975b74cddc3585826eed33973536
shilivvi <vanya.ivanov161@gmail.com>
2023-02-17 16:44:55 +0300

    Updated text block section

--- blocks/text-block-section/text-block-section.php
+++ blocks/text-block-section/text-block-section.php
@@ -1,24 +1,11 @@
 <section class="text-block">
     <div class="text-block__container container">
-        <h2 class="text-block__title title">Ora… probabilmente ti starai chiedendo qual è il prezzo per ricevere
-            il servizio di consulenza tesi e scrittura da parte dei nostri esperti.</h2>
+        <h2 class="text-block__title title">
+            <?php the_field('title'); ?>
+        </h2>
         <div class="text-block__grid">
             <div class="text-block__content">
-                <p>La consegna dell’elaborato avviene in 90 giorni e sia per le tesi compilative che per quelle
-                    sperimentali per le seguenti facoltà:</p>
-                <ul>
-                    <li>Medicina</li>
-                    <li>Economia</li>
-                    <li>Giurisprudenza</li>
-                    <li>Indirizzi</li>
-                </ul>
-                <p>Il prezzo è a partire da 33€/per pagina. Nel servizio di assistenza integrale tesi avrai
-                    anche a disposizione altri servizi, come:</p>
-                <ol>
-                    <li>Incontro audio on-line con l'autore, 30 minuti.</li>
-                    <li>Elaborazione di altre pagine: pagina del titolo, bibliografia tesi, sitografia.</li>
-                    <li>Controllo dell'unicità.</li>
-                </ol>
+                <?php the_field('text'); ?>
             </div>
             <div class="text-block__img">
                 <?php
