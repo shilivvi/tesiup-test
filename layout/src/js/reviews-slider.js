aa04af0f96fead509c550bc9f8b30aa297aa4a97
shilivvi <vanya.ivanov161@gmail.com>
2023-02-18 15:31:23 +0300

    Added blog section

--- layout/src/js/reviews-slider.js
+++ layout/src/js/reviews-slider.js
@@ -30,4 +30,28 @@ addEventListener('DOMContentLoaded', () => {
             }
         }
     });
+
+    tns({
+        container: '#blog-slider',
+        items: 1,
+        slideBy: 'page',
+        gutter: 15,
+        mouseDrag: true,
+        navPosition: 'bottom',
+        prevButton: '.reviews__arrow--prev',
+        nextButton: '.reviews__arrow--next',
+        loop: false,
+        swipeAngle: false,
+        responsive: {
+            1200: {
+                items: 3,
+            },
+            768: {
+                items: 2,
+            },
+            576: {
+                gutter: 25
+            }
+        }
+    });
 });
