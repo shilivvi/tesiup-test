a6fee3fac794531c5f1c3a78339b47b1d320f07f
shilivvi <vanya.ivanov161@gmail.com>
2023-02-20 09:26:56 +0300

    Added footer settings

--- inc/options-page.php
+++ inc/options-page.php
@@ -1,11 +1,17 @@
 <?php
 
 if( function_exists('acf_add_options_page') ) {
-    acf_add_options_page(array(
+    $parent = acf_add_options_page(array(
         'page_title'    => 'Theme Settings',
         'menu_title'    => 'Theme Settings',
         'menu_slug'     => 'theme-settings',
         'capability'    => 'edit_posts',
         'redirect'      => false
+    ));
+
+    acf_add_options_sub_page(array(
+        'page_title'  => 'Footer settings',
+        'menu_title'  => 'Footer',
+        'parent_slug' => $parent['menu_slug'],
     ));
 }
