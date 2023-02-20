1fdaa073cafabbbea68247dcf338ff83f3519bb4
shilivvi <vanya.ivanov161@gmail.com>
2023-02-20 16:17:27 +0300

    Added form section

---
+++ blocks/form-section/form-section.php
@@ -0,0 +1,8 @@
+<section class="form">
+    <div class="form__container container">
+        <h2 class="form__title title">Richiedete un preventivo gratuito per il successo</h2>
+        <div class="form__wrap">
+            <?php echo do_shortcode('[forminator_form id="374"]'); ?>
+        </div>
+    </div>
+</section>
